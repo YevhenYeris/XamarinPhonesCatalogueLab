0d61097233519708667c8e19bb8f7a119b38791c
Yevhen Yeris <yevhen_yeris@epam.com>
2022-12-09 00:30:54 +0200

    Add project files.

---
+++ PhonesDB/MainActivity.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+using PhonesDB.DB;
+using Android.Widget;
+using System.Linq;
+using Toolbar = AndroidX.AppCompat.Widget.Toolbar;
+
+namespace PhonesDB
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private PhonesDBHelper _dBHelper;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            var fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            InitDB();
+            DisplayAllPhones();
+        }
+
+        private void InitDB()
+        {
+            _dBHelper = new PhonesDBHelper(this);
+            //_dBHelper.OnCreate(_dBHelper.WritableDatabase);
+        }
+
+        private void DisplayAllPhones()
+        {
+            var phones = _dBHelper.GetPhones("Motorola", 5);
+            var list = FindViewById<ListView>(Resource.Id.list);
+            var arrayAdapter = new ArrayAdapter<String>(this, Resource.Layout.support_simple_spinner_dropdown_item, phones.Select(p => p.Model).ToArray());
+            list.Adapter = arrayAdapter;
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (View.IOnClickListener)null).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+}
